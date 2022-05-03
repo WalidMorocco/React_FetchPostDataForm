28a67af940d0a9dcf4d3a64d4fa061c7c346eac7
Walid Amar <walidamarpro@gmail.com>
2022-05-03 17:05:17 -0400

    activity

---
+++ src/components/PostingData.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import useFetch from '../hooks/useFetch';
+
+const PostingData = () => {
+
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [userId, setUserId] = useState(4);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const product = { userId, title, body };
+    
+        fetch('http://localhost:3000/posts/', {
+          method: 'POST',
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(product)
+        }).then(() => {
+          console.log('new product added');
+        })
+    }
+
+    const clearForm = (event) => {
+        event.preventDefault();
+    }
+
+
+    return (
+        <div className="form">
+            <h2>Add a New product</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label className="form-label">Title: </label>
+                    <input
+                        type="text"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                </div>
+                
+                <div className="form-group">
+                    <label className="form-label">Body: </label>
+                    <input
+                        type="text"
+                        value={body}
+                        onChange={(e) => setBody(e.target.value)}
+                    />
+                </div>
+
+                <div className="form-group">
+                    <button onClick={clearForm} className="button">
+                        Submit
+                    </button>
+                </div>   
+            </form>
+        </div>
+  )
+};
+
+export default PostingData;
