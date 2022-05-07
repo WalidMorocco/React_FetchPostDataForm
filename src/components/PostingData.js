84a8a3be89c50d777f4806ff978ce5ac823bae69
Walid Amar <walidamarpro@gmail.com>
2022-05-07 13:43:33 -0400

    post working no hook

--- src/components/PostingData.js
+++ src/components/PostingData.js
@@ -4,38 +4,37 @@ import usePost from '../hooks/usePost';
 
 const PostingData = () => {
 
-    const [userId, setUserId] = useState(11);
+
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
-    
-    
-    
-    const SubmitForm = e => {
-        e.preventDefault()
-        const input = {
-            userId:userId,
-            title: title,
-            body: body
-        }
-        console.log(input);
+    const [userId, setUserId] = useState(4);
 
-        const { data, error, loading } = usePost('posts', input);
+   
+
+    const HandleSubmit = (e) => {
+        e.preventDefault();
+        const product = { userId, title, body };
     
-        
-        if (loading) {
-            return <Loading />;
-        }
-    
-        if (error.length > 0) {
-            return <p>{error}</p>;
-        }
-        
+        fetch('http://localhost:3000/posts/', {
+          method: 'POST',
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(product)
+        }).then(() => {
+          console.log('new product added');
+        })   
+
     }
+
+    const clearForm = (event) => {
+        //event.preventDefault();
+    }
+
+
 
     return (
         <div className="form">
             <h2>Add a New product</h2>
-            <form>
+            <form onSubmit={HandleSubmit}>
 
                 <div className="form-group">
                     <label className="form-label">Title: </label>
@@ -56,87 +55,14 @@ const PostingData = () => {
                 </div>
 
                 <div className="form-group">
-                    <button onClick={SubmitForm} className="button">
+                    <button className="button">
                         Submit
                     </button>
                 </div>   
 
             </form>
         </div>
-    )
-
-
-
-
-
-
-
-
-
-
-
-
-//     const [title, setTitle] = useState('');
-//     const [body, setBody] = useState('');
-//     const [userId, setUserId] = useState(4);
-
-//     function postData(props) {
-
-//         const { data, loading, error } = UsePost('posts', props.title, props.body, props.userId);
-//     }
-
-//     const HandleSubmit = () => {
-//         e.preventDefault();
-//         const product = { userId, title, body };
-    
-//         fetch('http://localhost:3000/posts/', {
-//           method: 'POST',
-//           headers: { "Content-Type": "application/json" },
-//           body: JSON.stringify(product)
-//         }).then(() => {
-//           console.log('new product added');
-//         })   
-
-//     }
-
-//     const clearForm = (event) => {
-//         //event.preventDefault();
-//     }
-
-
-
-//     return (
-//         <div className="form">
-//             <h2>Add a New product</h2>
-//             <form onSubmit={HandleSubmit}>
-
-//                 <div className="form-group">
-//                     <label className="form-label">Title: </label>
-//                     <input
-//                         type="text"
-//                         value={title}
-//                         onChange={(e) => setTitle(e.target.value)}
-//                     />
-//                 </div>
-                
-//                 <div className="form-group">
-//                     <label className="form-label">Body: </label>
-//                     <input
-//                         type="text"
-//                         value={body}
-//                         onChange={(e) => setBody(e.target.value)}
-//                     />
-//                 </div>
-
-//                 <div className="form-group">
-//                     <button className="button">
-//                         Submit
-//                     </button>
-//                 </div>   
-
-//             </form>
-//         </div>
-//   )
+  )
 };
 
 export default PostingData;
